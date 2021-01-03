27a75f775b8632446ade94f1c04501faabefc1fc
Gvidas <gvisis@gmail.com>
2021-01-03 02:43:24 +0200

    css and js

---
+++ 4-hidden-search-widget/script.js
@@ -0,0 +1,10 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    // sets the focus to the input!!!
+    input.focus();
+})
+
