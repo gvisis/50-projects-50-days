2890895c584fd9e3c435e100871197c8b3ebfd2e
Gvidas <gvisis@gmail.com>
2021-02-04 01:01:03 +0200

    password-generator

--- password-generator-ui/script.js
+++ password-generator-ui/script.js
@@ -15,6 +15,22 @@ const randomFunc = {
   symbol: getRandomSymbol
 }
 
+clipboardEl.addEventListener('click', () => {
+  const textArea = document.createElement('textarea');
+  const password = resultEl.innerText;
+
+  if (!password) { return };
+
+  textArea.value = password;
+  document.body.appendChild(textArea);
+
+  // selects everything in text area
+  textArea.select();
+  document.execCommand('copy');
+  textArea.remove();
+  alert('Password copied to clipboard!')
+})
+
 generateEl.addEventListener('click', () => {
 
   // Returns length value in number;
